e69104a60a3561cad1de29da010114260321bc9a
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-06-04 16:21:58 +0300

    HTML AND CSS for login form

---
+++ ui.js
@@ -0,0 +1,8 @@
+const passwordInput  =  document.getElementById("passwordInput");
+const toggleBtn = document.getElementById("togglePassword");
+
+
+toggleBtn.addEventListener("click", () => {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
+})
