226ac5002b44de784f8113b2ad373def24c68a26
MikhailPotakovskiy <mihail.potakovskiy121@yandex.ru>
2025-06-12 08:39:09 +0300

    1

--- vendor/connect.php
+++ vendor/connect.php
@@ -5,11 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 try {
      $connect = new PDO("mysql:host=localhost;port=3306;dbname=test","root","");
-    // $connect = new PDO(
-    //     "mysql:host=sql204.hostinggem.ru;port=3306;dbname=gnioo_39196530_test;charset=utf8",
-    //     "gnioo_39196530",
-    //     "m2i0sh0a6"
-    // );
+
 
     $message = "Database connection established";
 } catch (PDOException $e) {
